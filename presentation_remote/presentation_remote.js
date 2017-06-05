4d90662aed1bbbca008b250ff663305875650966
Elton Viana <eltonvs@outlook.com>
2017-06-04 22:17:54 -0300

    Some improvements on Presentation Remote script

--- presentation_remote/presentation_remote.js
+++ presentation_remote/presentation_remote.js
@@ -1,38 +1,25 @@
-let util = require('util')
 let robot = require('robotjs')
-let async = require('async')
 let SensorTag = require('sensortag')
 
-console.log('Looking for near Sensor Tags...')
+console.log('>>> Looking for near Sensor Tags...')
 
 SensorTag.discover(sensorTag => {
+  // Bind Events
   sensorTag.on('disconnect', () => {
-    console.log('>>> disconnected')
+    console.log('>>> Disconnected')
     process.exit(0)
   })
-
-  console.log('discovered: ' + sensorTag)
-
-  async.series([
-    callback => {
-      sensorTag.connectAndSetUp(callback);
-    },
-    callback => {
-      console.log('receiving button press...')
-      sensorTag.on('simpleKeyChange', (left, right) => {
-        if (left && right) {
-          callback()
-        } else if (left) {
-          robot.keyTap('left')
-        } else if (right) {
-          robot.keyTap('right')
-        }
-      })
-      sensorTag.notifySimpleKey()
-    },
-    callback => {
-      console.log('disconnect...')
-      sensorTag.disconnect(callback)
+  sensorTag.on('simpleKeyChange', (left, right) => {
+    if (left && right) {
+      sensorTag.disconnect()
+    } else if (left) {
+      robot.keyTap('left')
+    } else if (right) {
+      robot.keyTap('right')
     }
-  ])
+  })
+
+  console.log('>>> Discovered: ' + sensorTag)
+
+  sensorTag.connectAndSetUp(() => sensorTag.notifySimpleKey())
 })
